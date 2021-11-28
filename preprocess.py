c1128e50ea51a7df799df90e7a463eed70f03421
Stephen Chen <stephen_r_chen@brown.edu>
2021-11-27 20:02:33 -0600

    deprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,8 @@
 
 import tensorflow as tf
 import numpy as np
-from music21 import converter, instrument, note, chord
+from music21 import *
+from fractions import Fraction
 
 # Hyperparameters
 SEQUENCE_LENGTH = 10
@@ -28,10 +29,10 @@ def process_instrument(instrument):
     if the instrument does not exist in the dictionary, it adds it first
     """
     if instrument in instrument_to_int_dict:
-        instrument_encoding = instrument_to_int_dict[instrument.instrumentName]
+        instrument_encoding = instrument_to_int_dict[instrument]
     else:
-        instrument_to_int_dict[instrument.instrumentName] = len(instrument_to_int_dict)
-        int_to_instrument_dict[len(instrument_to_int_dict)] = instrument.instrumentName
+        instrument_to_int_dict[instrument] = len(instrument_to_int_dict)
+        int_to_instrument_dict[len(instrument_to_int_dict)] = instrument
         instrument_encoding = len(instrument_to_int_dict)
     return instrument_encoding
 
@@ -66,21 +67,21 @@ def process_part(notes_to_parse):
         durations: a parallel list of durations, each duration is either a Fraction or float
         and represents the duration of the note in the same index
     """
-    notes = []
-    durations = []
-    currOffset = 0
+
+    size = int(round(float(notes_to_parse[-1].offset) * 12)) + 1
+    notes = [0] * size
+    durations = [0] * size
+    volumes = [0] * size
     for element in notes_to_parse:
         if (isinstance(element, note.Note)
         or isinstance(element, chord.Chord) 
         or isinstance(element, note.Rest)):
-            print(element.offset)
-            if(element.offset != currOffset):
-                notes.extend([0] * int((float(element.offset) - currOffset) * 12))
-            notes.append(process_element(element))
-            durations.append(float(element.duration.quarterLength))
-            currOffset = element.offset + 1/12
-    print(notes)
-    return (notes, durations)
+            index =  int(round(float(element.offset) * 12))
+            notes[index] = process_element(element)
+            durations[index] = float(element.duration.quarterLength)
+            if not (isinstance(element, note.Rest)):
+                volumes[index] = element.volume.velocity
+    return (notes, durations, volumes)
 
 def process_midi(filepath):
     """ 
@@ -102,20 +103,59 @@ def process_midi(filepath):
         for part in parts:
             instrument_encoding = process_instrument(part.getInstrument())
             notes_to_parse = part.recurse()
-            (notes, durations) = process_part(notes_to_parse)
-            score.append((instrument_encoding, notes, durations))
-            print(len(notes_to_parse))
+            (notes, durations, volumes) = process_part(notes_to_parse)
+            score.append((instrument_encoding, notes, durations, volumes))
             index = index + 1
         return score
     else: # file has notes in a flat structure
-        notes_to_parse = midi.flat.notes
+        notes_to_parse = midi.flat.notesAndRests
         instrument_encoding = process_instrument(parts.getInstrument())
-        (notes, durations) = process_part(notes_to_parse)
-        return [(instrument_encoding, notes, durations)]
-        
-def main():
-    midi = process_midi("midis/Red.mid")
-    print(midi)
+        (notes, durations, volumes) = process_part(notes_to_parse)
+        return [(instrument_encoding, notes, durations, volumes)]
 
+def deprocess_midi(encoding):
+    midi_score = stream.Score()
+    for part in encoding:
+        p = stream.Part()
+        p.insert(0, int_to_instrument_dict[part[0]])
+        p.insert(0, tempo.MetronomeMark(number = 120))
+        p.insert(0, meter.TimeSignature('4/4'))
+        offset = 0
+        for i in range(len(part[1])):
+            element_encoding = part[1][i]
+            element_string = int_to_element_dict[element_encoding]
+            if (element_string[0] == 'n'):
+                new_note = note.Note(element_string[2:])
+                new_duration = duration.Duration()
+                new_duration.quarterLength = part[2][i]
+                new_note.duration = new_duration
+                new_note.volume = part[3][i]
+                p.insert(Fraction(offset, 12), new_note)
+            elif (element_string[0] == 'r'):
+                new_rest = note.Rest()
+                new_duration = duration.Duration()
+                new_duration.quarterLength = part[2][i]
+                new_rest.duration = new_duration
+                p.insert(Fraction(offset, 12), new_rest)
+            elif (element_string[0] == 'c'):
+                notes_in_chord = element_string[2:].split('.')
+                notes = []
+                for current_note in notes_in_chord:
+                    new_note = note.Note(current_note)
+                    notes.append(new_note)
+                new_chord = chord.Chord(notes)
+                new_duration = duration.Duration()
+                new_duration.quarterLength = part[2][i]
+                new_chord.duration = new_duration
+                new_chord.volume = part[3][i]
+                p.insert(Fraction(offset, 12), new_chord)
+            offset = offset + 1
+        midi_score.insert(0, p)
+    return midi_score
+
+def main():
+    midi = process_midi("midis/red.mid")
+    midi_stream = deprocess_midi(midi)
+    midi_stream.write('midi', fp='test_output.mid')
 if __name__ == '__main__':
 	main()
