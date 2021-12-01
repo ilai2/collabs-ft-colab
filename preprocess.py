712d2cef2fc71ca8a11d44dae0aa22c0737d7aaf
Stephen Chen <stephen_r_chen@brown.edu>
2021-12-01 15:47:24 -0500

    for realsies 2

--- preprocess.py
+++ preprocess.py
@@ -324,7 +324,7 @@ def main():
     count = 0
     global element_to_int_dict
     global int_to_element_dict
-    _, int_to_element_dict = read_int_dict("asdf.txt")
+    _, int_to_element_dict = read_int_dict("dict.txt")
     (_, element_to_int_dict) = read_element_dict("dict.txt")
     
     for file in glob.glob(folder_name + "*.mid"):
