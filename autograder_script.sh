c2390c0599af91f8d6be7eb5c0dc77157d986523
ilai2 <isabel_lai@brown.edu>
2021-12-05 01:25:44 -0500

    did i fix it

--- autograder_script.sh
+++ autograder_script.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-curl https://bootstrap.pypa.io/get-pip.py | python3.9
+curl https://bootstrap.pypa.io/get-pip.py | python3.7
 pip install -r requirements.txt
 
-python3.9 model.py
+python3.7 model.py
 
 # have model.py write to output files, then you can use
 #    curl bashupload.com -T your_file.txt
