cb5a335e1caa5b53bebbe699920f2d2c7b93da6f
Jeremy Banuelos <jeremy@Jeremys-MacBook-Pro-2.local>
2021-12-09 01:36:18 -0500

    blues gospel

--- model.py
+++ model.py
@@ -167,14 +167,14 @@ def main():
 
     # load in 500 songs (17 taylor swift, 19 oned)
     for a in range(50):
-        pitches_f, durations_f, volumes_f = read_song('funk.txt', a)
+        pitches_f, durations_f, volumes_f = read_song('blues.txt', a)
         pitches_f = pitches_f[13]
         durations_f = durations_f[13]
         volumes_f = volumes_f[13]
         pitches.append(pitches_f)
         durations.append(durations_f)
         volumes.append(volumes_f)
-        pitches_s, durations_s, volumes_s = read_song('folk.txt', a)
+        pitches_s, durations_s, volumes_s = read_song('christian-gospel.txt', a)
         pitches_s = pitches_s[13]
         durations_s = durations_s[13]
         volumes_s = volumes_s[13]
@@ -264,7 +264,7 @@ def main():
 
     _, idict = read_int_dict("dict.txt")
     midi_score = deprocess_midi(score_pitches, score_durations, score_volumes, idict)
-    midi_out = midi_score.write('midi', fp='test_good_music_funkfolk.mid')
+    midi_out = midi_score.write('midi', fp='test_good_music_bluesgospel.mid')
     #model.save_weights("weights")
 
 if __name__ == '__main__':
